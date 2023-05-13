d22c41adb76cbd8df40072eef2ca1810ad444aae
GameDev1234 <zamannadeem990@gmail.com>
2023-05-13 21:18:02 +0500

    Add Horizontal Axis

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
 
     public float speed;
     public float turnspeed;
+    public float HorizontalInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +17,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        HorizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        transform.Translate(Vector3.right * Time.deltaTime * turnspeed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnspeed * HorizontalInput);
+
     }
 }
